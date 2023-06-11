db90575cf39743e029bc2ee3ff3fec67f41ebea3
Ludo <tech@soritra.com>
2023-06-11 21:08:15 +0000

    cleaned up unnecessary code

--- src/components/Main.d.tsx
+++ src/components/Main.d.tsx
@@ -2,7 +2,7 @@ import 'react';
 
 declare module 'react' {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-    availableTimes?: Array<string>;
+    availabletimes?: Array<string>;
     setAvailableTimes?: Dispatch<SetStateAction<Array<string>>>;
   }
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,67 +5,7 @@ import Testimonials from './Testimonials';
 import About from './About';
 
 
-// interface MainProps {
-//   children?: ReactNode,
-//   style?: CSSProperties
-// }
-
-// interface MainProps {
-//   availableTimes?: Array<string>
-// }
-
-// type MainProps = {
-//   availableTimes?: Array<string>;
-//   children?: ReactNode;
-// };
-
-// // our Button component
-// const Main2 = ({ availableTimes, children }: MainProps) => {
-//   return <main availableTimes={availableTimes}>{children}</main>;
-// }
-
-// type MainProps = JSX.IntrinsicElements["main"] & {
-//   readonly availableTimes?: Array<string>;
-// };
-
 const allSlots = Array.from({ length: 6 }, (val, k) => `${17 + k}:00`);
-
-// const MainComp: React.FunctionComponent<MainProps> = ({children, style, ...rest}) => {
-//   return (
-//     <main style={style} {...rest} availableTimes={availableTimes}>
-//       {children}
-//     </main>
-//   );
-// };
-
-// const Main2: FC<MainProps> = ({ availableTimes = [], ...props }) => (
-//   <main
-//     availableTimes={availableTimes}
-//     {...props}
-//   />
-// );
-
-// declare module 'react' {
-//   interface HTMLProps<T> {
-//     availableTimes?: Array<string>;
-//  }
-
-// }
-
-// const Main = ({ as: Component = 'main', ...rest }) => {
-//   const [availableTimes, setAvailableTimes] = useState(allSlots);
-
-//   if (JSON.stringify(rest) != '{}') {
-//     return <Component {...rest} />;
-//   }
-//   return (
-//     <main>
-//       <Specials />
-//       <Testimonials />
-//       <About />
-//     </main>
-//   );
-// };
 
 interface MainProps extends React.ComponentPropsWithRef<'main'> {
   children?: ReactNode,
@@ -77,7 +17,7 @@ function Main({children, style}: MainProps) {
 
   if (children) {
     return (
-      <main style={style} availableTimes={availableTimes}>
+      <main style={style} availabletimes={availableTimes}>
         {children}
       </main>
     )
